77459cf2be2a065538a199839fe9d4fd9c6e9c44
Jonas Ermert <ermert.jonas@yahoo.de>
2021-09-06 13:31:19 +0200

    detail-view Component under Core/components created
    C:\Users\Jonas\Desktop\Angular\AngularLastFM>ng g c detail-view
    CREATE src/app/detail-view/detail-view.component.html (26 bytes)
    CREATE src/app/detail-view/detail-view.component.spec.ts (655 bytes)
    CREATE src/app/detail-view/detail-view.component.ts (294 bytes)
    CREATE src/app/detail-view/detail-view.component.css (0 bytes)
    UPDATE src/app/app.module.ts (770 bytes)
    
    Folder Shared/components created
    Folder Shared/components/navigation and Shared/components/search created
    
    Files created:
    navigation.component.html
    search.component.html
    navigation.component.ts
    search.component.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './Core/components/home/home.component';
 import { MainViewComponent } from './Core/components/main-view/main-view.component';
+import { DetailViewComponent } from './Core/components/detail-view/detail-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    MainViewComponent
+    MainViewComponent,
+    DetailViewComponent
   ],
   imports: [
     BrowserModule,
