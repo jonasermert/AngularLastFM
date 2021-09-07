7060d3a826707413ad9bd8fc0d8cf4f84522bddb
Jonas Ermert <ermert.jonas@yahoo.de>
2021-09-07 15:18:15 +0200

    app.module.ts adapted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HomeComponent } from './Core/components/home/home.component';
 import { MainViewComponent } from './Core/components/main-view/main-view.component';
 import { DetailViewComponent } from './Core/components/detail-view/detail-view.component';
 import {HeaderComponent} from "./header.component";
+import {FooterComponent} from "./Shared/components/footer/footer.component";
 
 @NgModule({
   declarations: [
