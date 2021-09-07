191daa9f6532f8609929af259edd4cad0eaafe9d
Jonas Ermert <ermert.jonas@yahoo.de>
2021-09-07 17:31:03 +0200

    Different changes

--- src/app/Routing/app-routing.module.ts
+++ src/app/Routing/app-routing.module.ts
@@ -1,11 +1,1 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import {HomeComponent}  from "../Core/components/home/home.component";
 
-const routes: Routes = [];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
--- src/app/Shared/services/api.service.ts
+++ src/app/Shared/services/api.service.ts
@@ -12,7 +12,7 @@ export class ApiService {
 
     constructor(private http: HttpClient) { }
 
-searchMusic(Artist.Name: string, queryType: string) {
+searchMusic(Artist.Name:string, queryType: string) {
   return this.http.get(`http://ws.audioscrobbler.com/2.0/?method=artist.${queryType}&artist=${artistName}&api_key=${this.API_KEY_LASTFM}&format=json`);
 }
 
@@ -30,20 +30,20 @@ LastFM.prototype.searchArtists = function (artist, options) {
   var _this = this;
   if (options === void 0) { options = {}; }
   return this._searchArtists.apply(this, arguments)
-    .map(function (data) { return _this.validateData(data, 'results.artistmatches.artist', []); })
-    .map(function (artists) {
+    .map(function (data: any) { return _this.validateData(data, 'results.artistmatches.artist', []); })
+    .map(function (artists: any[]) {
       return artists
         .filter(function (artist) { return _this.checkUsableImage(artist); })
         .map(function (artist) { return Artist.fromJSON(artist); });
     });
 };
 
-LastFM.prototype.getTopArtists = function (options) {
+LastFM.prototype.getTopArtists = function (options: {} | undefined) {
   var _this = this;
   if (options === void 0) { options = {}; }
   return this._getTopArtists.apply(this, arguments)
-    .map(function (data) { return _this.validateData(data, 'artists.artist'); })
-    .map(function (artists) {
+    .map(function (data: any) { return _this.validateData(data, 'artists.artist'); })
+    .map(function (artists: any[]) {
       return artists
         .map(function (artist) { return Artist.fromJSON(artist); });
     });
