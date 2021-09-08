02a25ca77ee8221d156aa5080af14a9d4a50ecbd
Jonas Ermert <ermert.jonas@yahoo.de>
2021-09-08 13:57:04 +0200

    add stuff to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './Routing/app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './Core/components/home/home.component';
-import { MainViewComponent } from './Core/components/main-view/main-view.component';
+import {ArtistListComponent, MainViewComponent} from './Core/components/main-view/main-view.component';
 import { DetailViewComponent } from './Core/components/detail-view/detail-view.component';
 import {HeaderComponent} from "./Shared/components/header/header.component";
 import {FooterComponent} from "./Shared/components/footer/footer.component";
+import {SearchComponent} from "./Shared/components/search/ search.component";
+import {SharedModule} from "./Shared/shared.modules";
 
 @NgModule({
   declarations: [
@@ -17,12 +19,15 @@ import {FooterComponent} from "./Shared/components/footer/footer.component";
     HomeComponent,
     MainViewComponent,
     DetailViewComponent,
-    FooterComponent
+    FooterComponent,
+    ArtistListComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
